bfa45e887615847d62ac67fb05bf76206054e7c8
lindsayRae <lbarnett712@gmail.com>
2020-10-14 06:39:55 -0400

    fix- changed data to array so map() would work

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors())
 
 
 app.get('/', (req, res) => {
-  res.json({ id: 1, title: 'Hello from backend...'})
+  res.json([{ id: 1, title: 'Hello from backend...'},{ id: 1, title: 'Hello again from backend...'},])
 })
 
 app.listen(port, () => {
